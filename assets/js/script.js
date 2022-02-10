8e945589027431dc48345ca1cda26b8c99db1ecd
evs95 <elamarthy95@gmail.com>
2022-02-09 21:22:28 -0500

    store city list logic update

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,126 +12,128 @@ var searchedCities = [];
 // Ask user for location access to populate current location weather
 var userCoordinates = navigator.geolocation.getCurrentPosition(success, fail);
 
-function success (position){
-    var url = "https://api.openweathermap.org/data/2.5/weather?lat="+position.coords.latitude+
-                "&lon="+position.coords.longitude+"&units=imperial&appid=75cb326e22036d2782293ee5a922582b";
+function success(position) {
+    var url = "https://api.openweathermap.org/data/2.5/weather?lat=" + position.coords.latitude +
+        "&lon=" + position.coords.longitude + "&units=imperial&appid=75cb326e22036d2782293ee5a922582b";
     fetch(url)
         .then(function (response) {
-        return response.json();
-    })
-    .then(function(data){
-        cityEl.text(data.name+'('+moment().format("MM/DD/YYYY")+')');
-        $("#img").attr("src"," http://openweathermap.org/img/wn/"+data.weather[0].icon+".png");
-        tempEl.text("Temp: "+data.main.temp+"°F");
-        windEl.text("Wind: "+data.wind.speed+" MPH");
-        humidityEl.text("Humidity: "+data.main.humidity+" %");
-        UVIndexEl.text(0);
-        UVIndexEl.addClass(getColorForIndex(0));
-  });
+            return response.json();
+        })
+        .then(function (data) {
+            cityEl.text(data.name + '(' + moment().format("MM/DD/YYYY") + ')');
+            $("#img").attr("src", " http://openweathermap.org/img/wn/" + data.weather[0].icon + ".png");
+            tempEl.text("Temp: " + data.main.temp + "°F");
+            windEl.text("Wind: " + data.wind.speed + " MPH");
+            humidityEl.text("Humidity: " + data.main.humidity + " %");
+            UVIndexEl.text(0);
+            UVIndexEl.addClass(getColorForIndex(0));
+        });
 }
 
-function fail (fail){
+function fail(fail) {
     console.log(fail);
 }
 
 //Get weather data when user searches with city name
-function getWeatherData(city){
+function getWeatherData(city) {
     UVIndexEl.removeClass('bg-success');
     UVIndexEl.removeClass('bg-warning');
     UVIndexEl.removeClass('bg-danger');
 
-    var url = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&units=imperial&appid=75cb326e22036d2782293ee5a922582b";
+    var url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=75cb326e22036d2782293ee5a922582b";
     fetch(url)
         .then(function (response1) {
-        return response1.json();
-    })
-    .then(function(data1){
-        if(data1 !=null && data1.cod == 200){
-            var url = "https://api.openweathermap.org/data/2.5/onecall?lat="+data1.coord.lat+
-                "&lon="+data1.coord.lon+"&units=imperial&exclude=minutely,hourly&appid=75cb326e22036d2782293ee5a922582b";
-            fetch(url)
-                .then(function (response) {
-                return response.json();
-            })
-            .then(function(data){
-                cityEl.text(data1.name+'('+moment().format("MM/DD/YYYY")+')');
-                $("#img").attr("src"," http://openweathermap.org/img/wn/"+data1.weather[0].icon+".png");
-                tempEl.text("Temp: "+data1.main.temp+"°F");
-                windEl.text("Wind: "+data1.wind.speed+" MPH");
-                humidityEl.text("Humidity: "+data1.main.humidity+" %");
+            return response1.json();
+        })
+        .then(function (data1) {
+            if (data1 != null && data1.cod == 200) {
+                var url = "https://api.openweathermap.org/data/2.5/onecall?lat=" + data1.coord.lat +
+                    "&lon=" + data1.coord.lon + "&units=imperial&exclude=minutely,hourly&appid=75cb326e22036d2782293ee5a922582b";
+                fetch(url)
+                    .then(function (response) {
+                        return response.json();
+                    })
+                    .then(function (data) {
+                        cityEl.text(data1.name + '(' + moment().format("MM/DD/YYYY") + ')');
+                        $("#img").attr("src", " http://openweathermap.org/img/wn/" + data1.weather[0].icon + ".png");
+                        tempEl.text("Temp: " + data1.main.temp + "°F");
+                        windEl.text("Wind: " + data1.wind.speed + " MPH");
+                        humidityEl.text("Humidity: " + data1.main.humidity + " %");
 
-                if(data !=null && data.current != undefined && data.daily != undefined){
-                    UVIndexEl.text(data.current.uvi);
-                    UVIndexEl.addClass(getColorForIndex(data.current.uvi));
-                    loadFivedayForecast(data.daily);
-                }
-                else{
-                    showErrorMsg();
-                }
-                
-                loadSavedCities();
-            })
-            .catch(function (error) {
+                        if (data != null && data.current != undefined && data.daily != undefined) {
+                            UVIndexEl.text(data.current.uvi);
+                            UVIndexEl.addClass(getColorForIndex(data.current.uvi));
+                            loadFivedayForecast(data.daily);
+                        }
+                        else {
+                            showErrorMsg();
+                        }
+
+                        loadSavedCities();
+                    })
+                    .catch(function (error) {
+                        showErrorMsg();
+                    });
+
+            }
+            else {
                 showErrorMsg();
-              });
-            
-        }
-        else{
-            showErrorMsg();
-        }   
-  });
+            }
+        });
 };
 
-function showErrorMsg(){
+function showErrorMsg() {
     console.log("Error from API.")
 }
 
-function getColorForIndex(index){
-    return index>7?'bg-danger':index>2?'bg-warning':'bg-success';
+function getColorForIndex(index) {
+    return index > 7 ? 'bg-danger' : index > 2 ? 'bg-warning' : 'bg-success';
 }
 
-function loadFivedayForecast(forecastList){
+function loadFivedayForecast(forecastList) {
     forecastListEl.empty();
-    for(var i=1;i<=5;i++){
+    for (var i = 1; i <= 5; i++) {
         forecastListEl.append("<li class='col-2 mr-5 py-2 pb-5'><h2>"
-            +moment.unix(forecastList[i].dt).format("MM/DD/YYYY")+"</h2><div><img src='http://openweathermap.org/img/wn/"
-            +forecastList[i].weather[0].icon+".png'/></div><p>Temp:"+forecastList[i].temp.day+"°F</p><p>Wind: "
-            +forecastList[i].wind_speed +"MPH</p><p >Humidity:"+forecastList[i].humidity +"%</p></li>")
+            + moment.unix(forecastList[i].dt).format("MM/DD/YYYY") + "</h2><div><img src='http://openweathermap.org/img/wn/"
+            + forecastList[i].weather[0].icon + ".png'/></div><p>Temp:" + forecastList[i].temp.day + "°F</p><p>Wind: "
+            + forecastList[i].wind_speed + "MPH</p><p >Humidity:" + forecastList[i].humidity + "%</p></li>")
     }
 }
 
 var onSearchClick = function (event) {
     $('.alert').alert('close');
     event.preventDefault();
-    searchedCities=[];
+    searchedCities = [];
 
     var savedCitylist = localStorage.getItem("cityList");
-    if(savedCitylist != null && savedCitylist != ""){
+    if (savedCitylist != null && savedCitylist != "") {
         searchedCities = JSON.parse(savedCitylist);
     }
 
-    searchedCities.push(searchInputEl.val());
-    localStorage.setItem("cityList",JSON.stringify(searchedCities));
+    if (!searchedCities.includes(searchInputEl.val())) {
+        searchedCities.push(searchInputEl.val());
+        localStorage.setItem("cityList", JSON.stringify(searchedCities));
+    }
 
     getWeatherData(searchInputEl.val());
     searchInputEl.val('');
 };
 
-function onCityClick(event){
+function onCityClick(event) {
     event.preventDefault();
-    
+
     getWeatherData(this.textContent);
 };
 
-function loadSavedCities(){
-    searchedCities=[];
+function loadSavedCities() {
+    searchedCities = [];
     searchedCitiesListEl.empty();
 
     var savedCitylist = localStorage.getItem("cityList");
-    if(savedCitylist != null && savedCitylist != ""){
+    if (savedCitylist != null && savedCitylist != "") {
         searchedCities = JSON.parse(savedCitylist);
         searchedCities.forEach(element => {
-            var searchedCityBtn = $('<button class="btn w-100 my-3 searchedCity">'+element+'</button>')
+            var searchedCityBtn = $('<button class="btn w-100 my-3 searchedCity">' + element + '</button>')
             searchedCitiesListEl.append(searchedCityBtn);
         });
         var searchedCityBtnEl = $('.searchedCity');
